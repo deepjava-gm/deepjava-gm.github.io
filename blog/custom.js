1338162211e2bf23afbb34555d3fbab63122ca7c
gm <82674246@qq.com>
2024-05-10 22:08:54 +0800

    实现一个纯净阅读模式功能

--- blog/custom.js
+++ blog/custom.js
@@ -36,7 +36,7 @@ function showPopup() {
     popup.classList.add('popup'); // 添加一个类名
 
     popup.innerHTML = `
-    <div style="display: none; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: #fff; padding: 20px; border: 1px solid #ccc; box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); z-index: 9999;">
+    <div style="border-radius: 5px;color: white;display: none; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: #666565; padding: 20px; border: 1px solid white; box-shadow: 0 0 20px rgba(0,0,0,0.3); z-index: 9999;">
       <h3>已开启纯净阅读模式,再次双击可取消！</h3>
       <button style=" display: block; margin: 0 auto; color: white; width: 80px;height: 40px ; background: #84e2b7; border:none ;border-radius: 5px" onclick="closePopup()">确定</button>
     </div>
